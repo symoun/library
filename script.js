5fe3fea3e33c2a7d3ca7715b3cab49d830572364
S1DCO <symoun_ong@dlsu.edu.ph>
2023-03-14 23:02:28 +0800

    rendering value from form added

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 "use strict";
+const form = document.querySelector(".form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
+const gridLibrary = document.getElementById("grid-library");
 
 let myLibrary = [];
 
@@ -18,4 +24,33 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function renderBook() {}
+function renderBook() {
+  gridLibrary.innerHTML = "";
+  let html = "";
+  myLibrary.forEach((book) => {
+    html += `
+        <div class = "book-items">
+          <div>Title: ${book.title}</div> 
+          <div>Author: ${book.author}</div>
+          <div>Pages: ${book.pages}</div>
+          <div>Read: ${book.read ? "Completed" : "Incomplete"}</div>
+        </div>
+`;
+  });
+  gridLibrary.insertAdjacentHTML("beforeend", html);
+}
+
+function clearForm() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  read.checked = false;
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const book = new Book(title.value, author.value, pages.value, read.checked);
+  addBookToLibrary(book);
+  renderBook();
+  clearForm();
+});

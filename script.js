0cc25f20f310362dc16cf58660a8ce04090c6d43
S1DCO <symoun_ong@dlsu.edu.ph>
2023-03-15 22:05:09 +0800

    need to add validation

--- script.js
+++ script.js
@@ -29,18 +29,24 @@ function addBookToLibrary(book) {
 function renderBook() {
   gridLibrary.innerHTML = "";
   let html = "";
-  myLibrary.forEach((book) => {
+  myLibrary.forEach((book, index, array) => {
     html += `
-        <div class = "book-items">
+        <div class = "book-items" data-index ="${index}">
           <div>Title: ${book.title}</div> 
           <div>Author: ${book.author}</div>
           <div>Pages: ${book.pages}</div>
           <div>Read: ${book.read ? "Completed" : "Incomplete"}</div>
-
+          <button class = "close-book-items" type = "button" >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close-thick</title><path d="M20 6.91L17.09 4L12 9.09L6.91 4L4 6.91L9.09 12L4 17.09L6.91 20L12 14.91L17.09 20L20 17.09L14.91 12L20 6.91Z" /></svg>
+          </button>
+          <button class = "change-read" type = "button">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style = "height = 1em; width: 1em;"><title>arrow-left-drop-circle</title><path d="M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M14,7L9,12L14,17V7Z" /></svg>
+Change</button>
         </div>
 `;
   });
   gridLibrary.insertAdjacentHTML("beforeend", html);
+  addListenerToLibrary();
 }
 
 function clearForm() {
@@ -61,6 +67,25 @@ function clickSubmitButton(e) {
 
 function formBGToggleHidden() {
   formBackground.classList.toggle("hidden");
+}
+
+function addListenerToLibrary() {
+  document.querySelectorAll(".close-book-items").forEach((el) => {
+    el.addEventListener("click", deleteBook);
+  });
+  document
+    .querySelectorAll(".change-read")
+    .forEach((el) => el.addEventListener("click", changeRead));
+}
+
+function deleteBook(e) {
+  myLibrary.splice(e.target.closest(".book-items[data-index]"), 1);
+  renderBook();
+}
+function changeRead(e) {
+  myLibrary[e.target.closest(".book-items[data-index]").dataset.index].read =
+    !myLibrary[e.target.closest(".book-items[data-index]").dataset.index].read;
+  renderBook();
 }
 
 function run() {
